12e1feb5bb4827cc271689c005fae539dfd35d97
Nishanth Shetty <nishanthspshetty@gmail.com>
2021-02-04 18:31:04 +0530

    refactorig

--- src/lib.rs
+++ src/lib.rs
@@ -1,81 +1,77 @@
-pub mod language{
+pub mod language {
 
-#[derive(Debug)]
-pub struct Token {
-	pub token_val :String,
-}
+    #[derive(Debug)]
+    pub struct Token {
+        pub token_val: String,
+    }
 
-pub fn lexer(text:&str) -> Vec<Token> {
-	let mut vec:Vec<Token> = Vec::new();
-	let mut token :Vec<char> = Vec::new();
-	
-	
-	for ch in text.chars(){
-		
-		match ch {
-			' '|'\n' => {
-				let tok:String= token.iter().cloned().collect();
-				if !tok.is_empty() && tok.len()>0 {
-					vec.push(Token{token_val:tok});
-				}
-				token.clear();
-				continue;
-			},
-			_ => {
-				token.push(ch);
-			},
-		}
+    pub fn lexer(text: &str) -> Vec<Token> {
+        let mut vec: Vec<Token> = Vec::new();
+        let mut token: Vec<char> = Vec::new();
 
-	}
-	let tok:String = token.iter().cloned().collect();
-	if !tok.is_empty() && tok.len()>0 {
-		vec.push(Token{token_val:tok});
-	}
-	vec
-}
+        for ch in text.chars() {
+            match ch {
+                ' ' | '\n' => {
+                    let tok: String = token.iter().cloned().collect();
+                    if !tok.is_empty() && tok.len() > 0 {
+                        vec.push(Token { token_val: tok });
+                    }
+                    token.clear();
+                    continue;
+                }
+                _ => {
+                    token.push(ch);
+                }
+            }
+        }
+        let tok: String = token.iter().cloned().collect();
+        if !tok.is_empty() && tok.len() > 0 {
+            vec.push(Token { token_val: tok });
+        }
+        vec
+    }
 
+    // checks to see if s is a number
+    fn is_number(s: &String) -> bool {
+        for i in s.chars() {
+            if !i.is_digit(10) {
+                //radix = decimal
+                return false;
+            }
+        }
+        true
+    }
 
-// checks to see if s is a number
-fn is_number(s:&String) -> bool {
-	for i in s.chars(){
-		if !i.is_digit(10){ //radix = decimal
-			return false;
-		}
-	}
-	true
-}
+    //parse string s to number
+    fn to_number(s: &String) -> i32 {
+        let s: i32 = s.parse().expect("Unable to parse to number");
+        s
+    }
 
-//parse string s to number
-fn to_number(s:&String) -> i32 {
-	let s:i32=s.parse().expect("Unable to parse to number");
-	s
-}
+    //map the operator/mnemonic to vm code
+    fn map_to_number(s: &String) -> i32 {
+        match s.as_str() {
+            "+" => 0x40000001,
+            "-" => 0x40000002,
+            "*" => 0x40000003,
+            "/" => 0x40000004,
+            _ => {
+                panic!("Inavlid instruction");
+            }
+        }
+    }
 
-//map the operator/mnemonic to vm code
-fn map_to_number(s:&String) -> i32 {
-	match s.as_str() {
-		"+" => 0x40000001,
-		"-" => 0x40000002,
-		"*" => 0x40000003,
-		"/" => 0x40000004,
-		_ => {panic!("Inavlid instruction"); 
-		},	
-	}
-}
-
-//parse the tokens into vm instructions
-pub fn parser(token:Vec<Token>) -> Vec<i32>{
-	let mut vec:Vec<i32> = Vec::new();	
-	for i in token{
-		 if is_number(&i.token_val) {
-			vec.push(to_number(&i.token_val));
-		 }else{
-		 	vec.push(map_to_number(&i.token_val));	
-		 }
-	
-		}
-	vec.push(0x40000000);
-	vec
-}
-
+    //parse the tokens into vm instructions
+    pub fn parser(token: Vec<Token>) -> Vec<i32> {
+        let mut vec: Vec<i32> = Vec::new();
+        for i in token {
+            if is_number(&i.token_val) {
+                vec.push(to_number(&i.token_val));
+            } else {
+                vec.push(map_to_number(&i.token_val));
+            }
+        }
+        vec.push(0x40000000);
+        vec
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,44 @@ extern crate byteorder;
 
 mod lib;
 
-use byteorder::{ByteOrder,LittleEndian};
-use std::io::BufReader;
+use byteorder::{ByteOrder, LittleEndian};
+use std::fs::File;
 use std::io::Read;
 use std::io::Write;
-use std::fs::File;
 
 use lib::language::*;
 
 fn main() {
-	
-	//collect the arguments
-	let argv:Vec<_> = std::env::args().collect();
+    //collect the arguments
+    let argv: Vec<_> = std::env::args().collect();
 
-	if argv.len() != 2 {
-		println!("File not provided");
-		println!(" USAGE : stack-langc file");
-		return;
-	}
+    if argv.len() != 2 {
+        println!("File not provided");
+        println!(" USAGE : stack-langc file");
+        return;
+    }
 
-	let filename = argv[1].clone();
-    let mut  input  = String::new();	
-    File::open(filename).expect("Failed to open file").read_to_string(&mut input ).expect("Failed to read file");
-	
+    let filename = argv[1].clone();
+    let mut input = String::new();
+    File::open(filename)
+        .expect("Failed to open file")
+        .read_to_string(&mut input)
+        .expect("Failed to read file");
+
     //tokenize the given input
-	let tok = lexer(input.as_str()); //pass as a &str
-	let parsed_data = parser(tok);
-	
-	//let mut file = OpenOptions::new().write(true).create(true).open("a.out");
-	let mut file = File::create("a.out").expect("Unable to create file to output");
-	
-	//write to file
-	for i in parsed_data {
-	//println!(" {:b} ",i);
-		let mut buf = [0;4];
-		
-		//convert to bytes 
-		LittleEndian::write_i32(&mut buf,i);
-		file.write(&buf).expect("Unable to write to file");
-	
-	}
+    let tok = lexer(input.as_str()); //pass as a &str
+    let parsed_data = parser(tok);
 
+    //let mut file = OpenOptions::new().write(true).create(true).open("a.out");
+    let mut file = File::create("a.out").expect("Unable to create file to output");
+
+    //write to file
+    for i in parsed_data {
+        //println!(" {:b} ",i);
+        let mut buf = [0; 4];
+
+        //convert to bytes
+        LittleEndian::write_i32(&mut buf, i);
+        file.write(&buf).expect("Unable to write to file");
+    }
 }
